a68d0a76a856ccb02231120374f5799d81747791
Sergio Pedro <seperool@gmail.com>
2022-01-06 22:45:49 -0300

    inserir comentarios e variaveis

---
+++ 1-conhecendoReRstudio.R
@@ -0,0 +1,31 @@
+# Primeiro contato com a linguagem R
+
+#comentarios:
+# para inserir comentario usar "#" antes.
+
+#variaveis:
+#para executar click sobre a linha ou selecione o cojunto de linhas a serem executadas
+#e precione Crtl + Enter
+
+#utilizar <- para criação de variavel 
+#e = para atribuição de função
+
+a <- 10
+a
+b = 5 #existem casos em que atribuição com "=" não funciona.
+b
+10 -> c #é possivel inverter o sentido da seta.
+c
+
+#contas com variaveis
+c <- a-b
+c
+
+#variavel string
+a <- "Heitor"
+b <- "João"
+c = a + b #ERRO, não são varivais numericas, usar funções.
+c
+
+#atalhos:
+# para limpar a tela: Ctrl + L
