59b7af06032893c5b8c371129a3ff9aca0ea176e
Sergio Pedro <seperool@gmail.com>
2022-01-09 11:14:37 -0300

    estudo de funções(help,summary,str_c), instalação de pacotes e  pacote - stringr

--- 1-conhecendoReRstudio.R
+++ 1-conhecendoReRstudio.R
@@ -27,5 +27,45 @@ b <- "João"
 c = a + b #ERRO, não são varivais numericas, usar funções.
 c
 
+#funções
+#funções fazem parte de pacotes
+#pacotes contem muitas funções e precisam estar instalados
+#não sendo um pacote padrão ele precisa estar instalado
+
+c <- c(a,b) #vetor
+c
+
+#help
+?c
+
+#outras forma de colocar string dentro de um vetor
+c<-c("Heitor", "João")
+c
+
+a<-c(10,5,15,20)
+a[1] #acessando primeira posição
+
+?summary #resumo da variavel: minimo, maximo, 1st qu., mean ...
+summary(a)
+summary(c)
+
+#função de um pacote não padrão
+?str_c #erro por causa de não estar instalado a biblioteca da função
+#como descobrir a biblioteca dessa função:
+#acessar: rdocumentation.org
+#pela função, acha a biblioteca
+
+#installando pacote
+install.packages("stringr")
+library(stringr) #chama o pacote instalado
+
+#depois de instalado e chamado o pacote, consigo usar o help
+?str_c #junta multiplas strings em uma string
+nome<-"Heitor"
+sobrenome<-"Silva"
+
+nome_completo<-str_c(nome, sobrenome,sep = " ")
+nome_completo
+
 #atalhos:
 # para limpar a tela: Ctrl + L
