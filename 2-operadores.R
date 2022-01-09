96cf19c2df1fafdf8000076c6f81f65adcd6385b
Sergio Pedro <seperool@gmail.com>
2022-01-09 16:24:44 -0300

    operadores basicos, de comparação e logicos

---
+++ 2-operadores.R
@@ -0,0 +1,62 @@
+#comparadores matematicos, logicos e de comparação
+
+#operações basicas
+
+#soma
+2 + 2
+
+#subtração
+4 - 6
+
+#divisão
+9 / 3
+
+#multiplicação
+2 * 4
+
+#potencia, duas formas
+4 ** 5
+4 ^ 5
+
+#---
+#comparações
+#igual
+5 == 6
+5 == 5
+
+#diferença
+5 != 6
+5 != 5
+
+#maior
+4 > 2
+4 > 6
+
+#menor
+3 < 2
+3 < 4
+
+#maior ou igual
+4 >= 5
+4 >= 4
+
+#menor ou igual
+6 <= 8
+6 <= 5
+
+#---
+#operadores logicos
+#E
+6 == 6 & 7 == 8
+6 == 6 & 7 != 8
+5 > 3 & 5 < 8
+
+#OU
+6 == 6 | 7 == 8
+6 == 6 | 7 != 8
+5 > 3 | 5 < 8
+
+#NEGAÇÃO
+#nega o resultado
+! 6 == 6
+! 4 > 2
