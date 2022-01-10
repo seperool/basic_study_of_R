8d1e675b41840c8833fe135a1ec94eddd7ead5d6
Sergio Pedro <seperool@gmail.com>
2022-01-10 08:56:56 -0300

    armazenamento caracter - tipo caracter

--- 3-TiposDeDados.R
+++ 3-TiposDeDados.R
@@ -25,3 +25,31 @@ numero1
 numero2 <- c(salario,horas)
 numero2
 #---
+
+#armazenamento de caractere
+
+#para criar o tipo caracter usar aspas duplas ou aspas simples
+nome_1 <- "Eduardo Abreu"
+nome_2 <- "Amanda Lopes"
+nome_3 <- "Eduardo Abreu"
+nome_4 <- "Eduardo  Abreu"
+
+
+#numero também se transforma em caracter se usado aspas
+idade <- "25"
+
+idade + 5 #erro, não pode operar caracter com numero
+
+nomes <- nome_1+nome_2 #erro, não pode operar caracter
+nomes <- c(nome_1,nome_2) #armazenar atraves de vetor
+nomes[1]
+nomes[2]
+
+#é possivel comparar caracteres
+nome_1 == nome_2
+nome_1 == nome_3
+nome_1 == nome_4 #false por conta do espaço em banco
+
+#tomar cuidado, todo o caracter é avaliado, incluindo os espaços em branco
+
+#---
